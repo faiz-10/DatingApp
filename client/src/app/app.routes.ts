02071ccb1b05da9f947d7599db17961a37d9a6b7
Faizan Ali <faizanpali10@gmail.com>
2025-07-13 19:14:32 +0530

    complete routing between components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { UserList } from '../features/users/user-list/user-list';
+import { UserDetailed } from '../features/users/user-detailed/user-detailed';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'users', component: UserList, canActivate: [authGuard] },
+            { path: 'users/:id', component: UserDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
+    
+    { path: '**', component: Home } // Wildcard route for a 404 page or redirect
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,45 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit
+export class App
 {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'Dating App';
-  protected users = signal<User[]>([]);
-
-  async ngOnInit() {
-    this.users.set(await this.getUsers())
-    this.setCurrentUser();
-    };
-
-    setCurrentUser() {
-      const userString = localStorage.getItem("user");
-      if(!userString) return;
-      const user = JSON.parse(userString);
-      this.accountService.currentUser.set(user);
-    }
-
-    async getUsers(){
-    try 
-    {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/user'));
-    } 
-    catch (error) 
-    {
-      console.log(error);
-      throw error;
-    }
-  }
+  protected router = inject(Router);
 }
